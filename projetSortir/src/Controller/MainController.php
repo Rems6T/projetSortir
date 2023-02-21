b50ae0b06505af728974d0c68fb6a0314a17acb2
Amélie <amelie.bouget2022@campus-eni.fr>
2023-02-21 11:25:43 +0100

    page Accueil

---
+++ projetSortir/src/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_main_index")
+     * @return Response
+     */
+    public function index(SortieRepository $SortieRepository): Response
+    {
+    $sorties = $SortieRepository->findAll();
+
+        return $this->render('main/index.html.twig',
+        [ // les passe à Twig "
+            "sorties" => $sorties, ]); }}
+
+
