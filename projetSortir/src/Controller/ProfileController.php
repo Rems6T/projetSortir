1fd224395b049e22ee7cfbcaa59d9138da1b954e
Rems6T <remi.tidda@gmail.com>
2023-02-22 15:01:48 +0100

    Création du CRUD pour les profiles
    Commande:
    php bin/console make:admin:crud
    Puis choisir le nom de l'entité utilisée enfin le nom du controller qui doit être créer pour le CRUD

---
+++ projetSortir/src/Controller/ProfileController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Participant;
+use App\Form\ParticipantType;
+use App\Repository\ParticipantRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/profile")
+ */
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_profile_index", methods={"GET"})
+     */
+    public function index(ParticipantRepository $participantRepository): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'participants' => $participantRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="app_profile_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, ParticipantRepository $participantRepository): Response
+    {
+        $participant = new Participant();
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $participantRepository->add($participant, true);
+
+            return $this->redirectToRoute('app_profile_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('profile/new.html.twig', [
+            'participant' => $participant,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_profile_show", methods={"GET"})
+     */
+    public function show(Participant $participant): Response
+    {
+        return $this->render('profile/show.html.twig', [
+            'participant' => $participant,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_profile_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Participant $participant, ParticipantRepository $participantRepository): Response
+    {
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $participantRepository->add($participant, true);
+
+            return $this->redirectToRoute('app_profile_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('profile/edit.html.twig', [
+            'participant' => $participant,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_profile_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Participant $participant, ParticipantRepository $participantRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$participant->getId(), $request->request->get('_token'))) {
+            $participantRepository->remove($participant, true);
+        }
+
+        return $this->redirectToRoute('app_profile_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
