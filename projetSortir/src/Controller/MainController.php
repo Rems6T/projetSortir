e24761b92eb06bcbb6a7f519cf16b70131c702f4
Amélie <amelie.bouget2022@campus-eni.fr>
2023-02-23 09:09:30 +0100

    Modif menu + filtres en cours

--- projetSortir/src/Controller/MainController.php
+++ projetSortir/src/Controller/MainController.php
@@ -11,24 +11,78 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    /**
-     * @Route("/", name="app_main_index")
-     * @return Response
-     */
-    public function index(SortieRepository $SortieRepository, CampusRepository $campusRepository,Request $request): Response
-    {
-       $campusS = $campusRepository->findAll();
-         $sorties = $SortieRepository->findAll();
-        if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['search'])){
-            $data = $request;
 
+       /**
+        * @Route("/", name="app_main_index")
+        */
+    public function index(SortieRepository $sortieRepo, CampusRepository $CampusRepository): Response
+    {
+        $sorties = $sortieRepo->findAllWithSitesAndEtats();
+        $campusS = $CampusRepository->findAll();
+        return $this->render('main/index.html.twig', [
+            'sorties' => $sorties,
+            'campusS' => $campusS,
+        ]);
+    }
+
+
+    /**
+     *@Route("/search/{id}",name="app_search")
+     */
+    public function search(Request $request, SortieRepository $SortieRepository, CampusRepository $CampusRepository, $id): Response
+
+    {
+        $search = $request->request->get('search');
+        $dateDebut = $request->request->get('dateMin');
+        $dateLimiteInscription = $request->request->get('dateMax');
+        $estOrganisateur = $request->request->get('est_organisateur');
+        $estInscrit = $request->request->get('est_inscrit');
+        $pasInscrit = $request->request->get('pas_inscrit');
+        $SortiesPassees =  $request->request->get('sortie_terminé');
+        $idCurrentUser = $id;
+        $idCampus = $request->request->get('campus');
+
+        $sorties = [];
+
+        if (
+            $search != "" && $estOrganisateur == null && $estInscrit == null && $pasInscrit == null
+            && $SortiesPassees == null && $dateDebut == "" && $dateLimiteInscription == ""
+        ) {
+            $sorties = array_merge($sorties, $SortieRepository->findBySearchAndCampus($search, $idCampus));
         }
 
-        return $this->render('main/index.html.twig',
-        [ // les passe à Twig "
-            "sorties" => $sorties,
-            'campusS' => $campusS
-            ]);
-    }}
+        if (
+            $search != "" && $estOrganisateur == null && $estInscrit == null && $pasInscrit == null
+            && $SortiesPassees == null && $dateDebut == "" && $dateLimiteInscription == ""
+         ) {
+            $sorties = array_merge($sorties, $SortieRepository->findByCampus($idCampus));
+         }
 
+        if ($dateDebut != "" && $dateLimiteInscription != "") {
+        $sorties = array_merge($sorties, $SortieRepository->findByDates($dateDebut, $dateLimiteInscription, $idCampus, $search));
+        }
 
+        if ($estOrganisateur != null) {
+        $sorties = array_merge($sorties, $SortieRepository->findByIdOrganisateur($idCurrentUser, $idCampus, $search));
+        }
+
+        if ($estInscrit != null) {
+         $sorties =  array_merge($sorties, $SortieRepository->findByIdParticipantInscrit($idCurrentUser, $idCampus, $search));
+        }
+
+        if ($pasInscrit != null) {
+        $sorties = array_merge($sorties, $SortieRepository->findByIdParticipantNonInscrit($SortieRepository->findAll(), $idCurrentUser, $idCampus));
+        }
+
+        if ($SortiesPassees != null) {
+        $sorties = array_merge($sorties, $SortieRepository->findByEtatPassees($idCampus, $search));
+        }
+
+        $sorties = array_unique($sorties, SORT_REGULAR);
+
+        $campusS = $CampusRepository->findAll();
+        return $this->render('main/index.html.twig', [
+            'sorties' => $sorties,
+           'campusS' => $campusS,
+        ]);
+        } }
