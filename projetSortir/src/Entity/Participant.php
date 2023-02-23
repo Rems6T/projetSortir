880a8653d90ba829ac113bf473efb3f9ac2fa7aa
Rems6T <remi.tidda@gmail.com>
2023-02-23 15:00:51 +0100

    Minor fixes
    Suppression des images de test
    Réglage dans le profil : affichage du pseudo en titre de la page

--- projetSortir/src/Entity/Participant.php
+++ projetSortir/src/Entity/Participant.php
@@ -63,7 +63,7 @@ class Participant implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @ORM\Column(type="boolean")
      */
-    private $actif = false;
+    private $actif;
 
     /**
      * @ORM\ManyToOne(targetEntity=Campus::class, inversedBy="participants")
@@ -240,6 +240,7 @@ class Participant implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
 
     public function isActif(): ?bool
     {
