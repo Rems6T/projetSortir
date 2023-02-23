8299e7bdc0ecb2be210a71cdc55355f4c98aae8e
man06 <manuelvalance@orange.fr>
2023-02-23 16:40:35 +0100

    barre recherche

--- projetSortir/src/Controller/MainController.php
+++ projetSortir/src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Participant;
 use App\Entity\Sortie;
 use App\Repository\CampusRepository;
+use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -32,56 +33,64 @@ class MainController extends AbstractController
     /**
      *@Route("/search/{id}",name="app_search")
      */
-    public function search(Request $request, SortieRepository $SortieRepository, CampusRepository $CampusRepository, $id): Response
+    public function search(Request $request, SortieRepository $SortieRepository, CampusRepository $CampusRepository, ParticipantRepository $participantRepository): Response
 
     {
-        $search = $request->request->get('search');
-        $dateMin = $request->request->get('dateMin');
-        $dateMax = $request->request->get('dateMax');
-        $estOrganisateur = $request->request->get('est_organisateur');
-        $estInscrit = $request->request->get('est_inscrit');
-        $pasInscrit = $request->request->get('pas_inscrit');
-        $SortiesPassees =  $request->request->get('sortie_terminé');
-        $idCurrentUser = $id;
-        $idCampus = $request->request->get('campus');
+//        $search = $request->request->get('search');
+//        $dateMin = $request->request->get('dateMin');
+//        $dateMax = $request->request->get('dateMax');
+//        $estOrganisateur = $request->request->get('est_organisateur');
+//        $estInscrit = $request->request->get('est_inscrit');
+//        $pasInscrit = $request->request->get('pas_inscrit');
+//        $SortiesPassees =  $request->request->get('sortie_terminé');
+//        $idCurrentUser = $id;
+//        $Campus = $CampusRepository->find($request->request->get('campus')) ;
+        $data = [];
+        $data['search'] = $request->get('search');
+        $data['dateMin'] = $request->get('dateMin');
+        $data['dateMax'] = $request->get('dateMax');
+        $data['est_organisateur'] = $request->get('est_organisateur');
+        $data['est_inscrit'] = $request->get('est_inscrit');
+        $data['pas_inscrit'] = $request->get('pas_inscrit');
+        $data['sortie_termine'] = $request->get('sortie_termine');
+        $data['user'] = $participantRepository->find($request->request->get('user'));
+        $data['campus'] = $CampusRepository->find($request->request->get('campus'));
+        $sorties = $SortieRepository->findByRecherche($data);
 
-        $sorties = [];
-
-        if (
-            $search != "" && $estOrganisateur == null && $estInscrit == null && $pasInscrit == null
-            && $SortiesPassees == null && $dateMin == "" && $dateMax == ""
-        ) {
-            $sorties = array_merge($sorties, $SortieRepository->findBySearch($search));
-        }
-
-        if (
-            $search != "" && $estOrganisateur == null && $estInscrit == null && $pasInscrit == null
-            && $SortiesPassees == null && $dateMin == "" && $dateMax == ""
-         ) {
-            $sorties = array_merge($sorties, $SortieRepository->findByCampus($idCampus));
-         }
-
-        if ($dateMin != "" && $dateMax != "") {
-        $sorties = array_merge($sorties, $SortieRepository->findByDates($dateMin, $dateMax, $idCampus, $search));
-        }
-
-        if ($estOrganisateur != null) {
-        $sorties = array_merge($sorties, $SortieRepository->findByIdOrganisateur($idCurrentUser, $idCampus, $search));
-        }
-
-        if ($estInscrit != null) {
-         $sorties =  array_merge($sorties, $SortieRepository->findByIdParticipantInscrit($idCurrentUser, $idCampus, $search));
-        }
-
-        if ($pasInscrit != null) {
-        $sorties = array_merge($sorties, $SortieRepository->findByIdParticipantNonInscrit($SortieRepository->findAll(), $idCurrentUser, $idCampus));
-        }
-
-        if ($SortiesPassees != null) {
-        $sorties = array_merge($sorties, $SortieRepository->findByEtatPassees($idCampus, $search));
-        }
-
-        $sorties = array_unique($sorties, SORT_REGULAR);
+//        if (
+//            $search != "" && $estOrganisateur == null && $estInscrit == null && $pasInscrit == null
+//            && $SortiesPassees == null && $dateMin == "" && $dateMax == ""
+//        ) {
+//            $sorties = array_merge($sorties, $SortieRepository->findBySearch($search));
+//        }
+//
+//        if (
+//            $Campus != null
+//         ) {
+//            $sorties = array_merge($sorties, $SortieRepository->findByCampus($Campus));
+//         }
+//
+//        if ($dateMin != "" && $dateMax != "") {
+//        $sorties = array_merge($sorties, $SortieRepository->findByDates($dateMin, $dateMax, $Campus, $search));
+//        }
+//
+//        if ($estOrganisateur != null) {
+//        $sorties = array_merge($sorties, $SortieRepository->findByIdOrganisateur($idCurrentUser, $Campus, $search));
+//        }
+//
+//        if ($estInscrit != null) {
+//         $sorties =  array_merge($sorties, $SortieRepository->findByIdParticipantInscrit($idCurrentUser, $Campus, $search));
+//        }
+//
+//        if ($pasInscrit != null) {
+//        $sorties = array_merge($sorties, $SortieRepository->findByIdParticipantNonInscrit($SortieRepository->findAll(), $idCurrentUser, $Campus));
+//        }
+//
+//        if ($SortiesPassees != null) {
+//        $sorties = array_merge($sorties, $SortieRepository->findByEtatPassees($Campus, $search));
+//        }
+//
+//        $sorties = array_unique($sorties, SORT_REGULAR);
 
         $campusS = $CampusRepository->findAll();
         return $this->render('main/index.html.twig', [
